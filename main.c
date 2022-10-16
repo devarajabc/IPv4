2085ddb7e39605f93a2acb881e08d8c729bbb557
chiu-chiu-kuan <devaraja1945@gmail.com>
2022-10-16 14:21:41 +0800

    didn't end up with '\n

--- main.c
+++ main.c
@@ -2,9 +2,9 @@
 
 int main () {
     int flag =0;
-    int dot=3;
+    int n=4;
     int deci16=0;
-    while(1)
+    while(n>0)//如果是大於一，n就不能是1了
     {
         int deci=0;
         int mychar=0;
@@ -12,12 +12,8 @@ int main () {
         ////////////////////////////
         if(mychar>='a'&&mychar<='f')//a~f
         {
-            deci =(mychar-87);
+            deci = (mychar - 87);
 
-        }
-        else if(mychar=='\n')
-        {
-            break;
         }
         else //0~9
         {
@@ -30,18 +26,18 @@ int main () {
             deci16=deci*16;
             flag++;
         }
-        else
+        else//second one
         {
             //16^0,and it's time to print it out
             printf("%d",deci+deci16);
             //printf("deci='%d\n",deci);
             //printf("deci6='%d\n",deci16);
-            if(dot>0)
+            if(n>1)
                 printf(".");
             flag=0;
             deci=0;
             deci16=0;
-            dot--;
+            n--;
         }
 
     }
